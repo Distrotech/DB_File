73a8bd0ee5d58193a994923cbb73ea3d4069e088
Greg Nietsky <gregory@distrotech.co.za>
2014-01-25 13:28:47 +0200

    Add x32 support to config script

--- config.sh
+++ config.sh
@@ -2,6 +2,7 @@
 
 case ${1} in
 	x86_64)sed -i -e "s/^\(LIB.*= \/usr\/lib\).*/\164\/x86_64/" config.in;;
+	x86_32)sed -i -e "s/^\(LIB.*= \/usr\/lib\).*/\1x32\/i686/" config.in;;
 	i[3-6]86)sed -i -e "s/^\(LIB.*= \/usr\/lib\).*/\1\/i686/" config.in;;
 	aarch64)sed -i -e "s/^\(LIB.*= \/usr\/lib\).*/\1/" config.in;;
 	*64)sed -i -e "s/^\(LIB.*= \/usr\/lib\).*/\1/64" config.in;;
